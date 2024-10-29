c259e3ce1d48c89c2e3206b4ebd64fa64ca33e55
Joaquin <joaquin.guillot.98@gmail.com>
2024-10-29 18:59:27 -0300

    Volviendo a versiones anteriores

--- src/main/java/com/example/mutantes/search/Nodo.java
+++
@@ -1,8 +0,0 @@
-package com.example.mutantes.search;
-import java.util.HashMap;
-import java.util.Map;
-
-public class Nodo {
-    Map<Character, Nodo> hijo = new HashMap<>();
-    boolean isEndOfWord = false;
-}
