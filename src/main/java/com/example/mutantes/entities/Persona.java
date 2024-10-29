cdf89318bef895b7cdbbfc15a18bf38a6704cc87
Joaquin <joaquin.guillot.98@gmail.com>
2024-10-29 19:03:45 -0300

    Volviendo a versiones anteriores

--- src/main/java/com/example/mutantes/entities/Persona.java
+++ src/main/java/com/example/mutantes/entities/Persona.java
@@ -24,32 +24,5 @@ public class Persona{
         @Column(name = "apellido")
         private String apellido;
 
-        private char[][] matriz;
-
-        private boolean esMutante;
-
-        public Persona(char[][] matriz) {
-            this.matriz = matriz;
-            this.esMutante = false; // Inicialmente, se asume que no contiene patrones
-        }
-
-    public boolean getEsMutante() {
-            return esMutante;
-        }
-
-    // Método para verificar si la matriz contiene algún patrón
-        public void checkForPatterns(ArbolService arbolService) {
-            // Aquí se puede usar la lógica que ya tienes en PatternSearchService
-            // para buscar patrones en la matriz y actualizar containsPattern.
-
-            // Ejemplo de cómo podrías hacerlo (asumiendo que tienes una lista de patrones):
-            String[] patterns = {"aaaa", "PATTERN2", "PATTERN3"}; // Reemplaza con tus patrones
-            for (String pattern : patterns) {
-                if (arbolService.searchWord(pattern)) {
-                    this.esMutante = true;
-                    break; // Si se encuentra un patrón, no es necesario seguir buscando
-                }
-            }
-        }
 
 }
